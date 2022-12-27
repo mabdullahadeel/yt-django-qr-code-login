002bb9de72e8eaa939b4c51d6f10d2f44f993095
Abdullah Adeel <easyautosol@gmail.com>
2022-12-27 05:16:43 +0500

    🎇 registerd new views

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import ObtainAuthToken
+from . import views as user_views
 
 urlpatterns = [
-    path("login/", ObtainAuthToken.as_view(), name="login")
+    path("login/", ObtainAuthToken.as_view(), name="login"),
+    path("register/", user_views.UserCreateView.as_view(), name="register"),
+    path("me/", user_views.UserMeView.as_view(), name="me"),
+
 ]
